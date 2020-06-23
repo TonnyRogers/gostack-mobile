201fccf472ff429e687df82f2b207246cf05161a
tonnyrogers <antoniel15975@gmail.com>
2020-06-23 17:11:38 -0300

    App Center / Push Notification / buildTypes

--- src/index.js
+++ src/index.js
@@ -1,23 +1,55 @@
-import React from 'react';
+import React, { Component } from 'react';
 import { NavigationContainer } from '@react-navigation/native';
 import { PersistGate } from 'redux-persist/integration/react';
 import { Provider } from 'react-redux';
 import { StatusBar } from 'react-native';
+import CodePush from 'react-native-code-push';
+import OneSignal from 'react-native-onesignal';
 
 import './config/ReactotronConfig';
 
 import { store, persistor } from './store';
 import App from './App';
 
-export default function Index() {
-  return (
-    <Provider store={store}>
-      <PersistGate persistor={persistor}>
-        <NavigationContainer>
-          <StatusBar backgroundColor="#7159c1" barStyle="light-content" />
-          <App />
-        </NavigationContainer>
-      </PersistGate>
-    </Provider>
-  );
+class Index extends Component {
+  constructor(props) {
+    super(props);
+
+    OneSignal.init('f689f879-3a01-4360-b69e-e25a838ca9f4');
+    OneSignal.addEventListener('received', this.onReceived);
+    OneSignal.addEventListener('opened', this.onOpened);
+    OneSignal.addEventListener('ids', this.onIds);
+  }
+
+  componentWillUnmount() {
+    OneSignal.removeEventListener('received', this.onReceived);
+    OneSignal.removeEventListener('opened', this.onOpened);
+    OneSignal.removeEventListener('ids', this.onIds);
+  }
+
+  // disparada quando app esta aberto
+  onReceived = (data) => {};
+
+  // disparada quando clicar numa notificação
+  onOpened = (notification) => {};
+
+  // quando usuario faz registro no serviço de notificaçao
+  onIds = (id) => {};
+
+  render() {
+    return (
+      <Provider store={store}>
+        <PersistGate persistor={persistor}>
+          <NavigationContainer>
+            <StatusBar backgroundColor="#7159c1" barStyle="light-content" />
+            <App />
+          </NavigationContainer>
+        </PersistGate>
+      </Provider>
+    );
+  }
 }
+
+export default CodePush({
+  checkFrequency: CodePush.CheckFrequency.ON_APP_RESUME,
+})(Index);
